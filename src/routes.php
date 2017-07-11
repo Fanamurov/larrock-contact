c7b747e508e124fb7d97a15a964f8cc6979107ec
Alexandr Fanamurov <fanamura@gmail.com>
2017-07-11 17:27:10 +1000

    Fix

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,18 @@
 
 use Larrock\ComponentContact\ContactController;
 
-Route::group(['middleware' => ['web', 'AddMenuFront', 'GetSeo', 'AddBlocksTemplate']], function(){
+$middlewares = ['web', 'GetSeo'];
+if(file_exists(base_path(). '/vendor/fanamurov/larrock-menu')){
+    $middlewares[] = 'AddMenuFront';
+}
+if(file_exists(base_path(). '/vendor/fanamurov/larrock-blocks')){
+    $middlewares[] = 'AddBlocksTemplate';
+}
+if(file_exists(base_path(). '/vendor/fanamurov/larrock-discounts')){
+    $middlewares[] = 'DiscountsShare';
+}
+
+Route::group(['middleware' => $middlewares], function(){
     Route::post('/forms/contact', [
         'as' => 'submit.contacts', 'uses' => ContactController::class .'@send_form'
     ]);
