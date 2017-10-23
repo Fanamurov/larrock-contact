8f3086136163a5911cca464ba40e4831c86e8d52
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-23 14:14:10 +1000

    small fix

--- src/views/admin/dashboard/formslog.blade.php
+++ src/views/admin/dashboard/formslog.blade.php
@@ -2,14 +2,13 @@
     <div class="uk-panel uk-alert">
         <p class="uk-h3"><a href="/admin/{{ $component->name }}">{{ $component->title }}</a></p>
         @if(count($data) > 0)
-            <p class="uk-margin-bottom-remove">Новые письма:</p>
-            <ul>
+            <ul class="uk-list">
                 @foreach($data as $value)
                     <li><a href="/admin/contact/{{ $value->id }}/edit">{{ $value->title }}</a>
                         @if($value->form_status === 'Новая')
-                            <span class="uk-badge uk-badge-warning">{{ $value->form_status }}</span>
+                            <span class="uk-badge uk-badge-warning">{{ \Carbon\Carbon::parse($value->created_at)->format('d M') }} {{ $value->form_status }}</span>
                         @else
-                            <span class="uk-badge uk-badge-notification">{{ $value->form_status }}</span>
+                            <span class="uk-badge uk-badge-notification">{{ \Carbon\Carbon::parse($value->updated_at)->format('d M') }} {{ $value->form_status }}</span>
                         @endif
                     </li>
                 @endforeach
