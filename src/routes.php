7ed3fdc17b333f910adb92ac7429be8afd6487ee
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-22 17:24:40 +1000

    hot fix

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::group(['middleware' => $middlewares], function(){
-    Route::any('/form/send/{param?}', 'Larrock\ComponentContact\ContactController@send_form')->name('send.form');
-});
+Route::any('/form/send/{param?}', 'Larrock\ComponentContact\ContactController@send_form')->name('send.form');
 
 Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
     Route::resource('contact', 'Larrock\ComponentContact\AdminContactController');
